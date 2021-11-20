ba7ba5d6658ae7b868a70211e13f11fbb9af00ca
Mhawar <Mhawar2018a@gmail.com>
2021-11-20 08:45:26 +0100

    stopped on the 5th selector

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ with sync_playwright() as p:
 
  page.fill('input#o3-input' , 'mhawar2018a@gmail.com')
  page.fill('input#o4-input' , 'mhawar2001')
+ page.click('#provider-mounter > div > div.zp_2Pnik > div.zp_2YB95 > div > div.zp_1QN2s > div > div.zp_p7Ra4.zp_2ZLGm > div > form > div.zp_18q8k > div > div')
  
  
- 
- for i in range (1,100):
-       page.click('#provider-mounter > div > div.zp_2Pnik > div.zp_2YB95 > div > div.zp_1QN2s > div > div.zp_p7Ra4.zp_2ZLGm > div > form > div.zp_18q8k > div > div')
+ for i in range (100):
+       
+       
        page.click('#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div.zp_3cHPT > div.zp_3Lzj1 > div > div > div > div.finder-explorer-sidebar-shown.zp_1cI0A.zp_iYGsF > div.zp_1ybjt > div > div.zp_mtt5S.zp_2xQfh.zp_vXURL > div > div:nth-child(1) > div.zp_3Lc6D > div > div > div')
+       print("clicked 1")
        page.click('body > div.apolloio-css-vars-reset.zp-overlay > div > div > div > a:nth-child(1)')
+       print(" 2")
        page.click('#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div.zp_3cHPT > div.zp_3Lzj1 > div > div > div > div.finder-explorer-sidebar-shown.zp_1cI0A.zp_iYGsF > div.zp_1ybjt > div > div.zp_mtt5S.zp_2xQfh.zp_vXURL > div > div:nth-child(1) > div.zp-button-group.zp_3tokH.zp_1JYqA > span:nth-child(4) > div')
+       print(" 3")
        page.click('body > div.apolloio-css-vars-reset.zp-overlay > div > div > div > a:nth-child(1)')
+       print(" 4")
        page.click('body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_3wjKN > form > div > div > div > div > div > div > div > div.zp-select-main > div')
+       print(" 5")
        page.click('body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_3wjKN > form > div > div > div > div > div > div > div.Select-menu-outer > div > div:nth-child(1)')
+       print(" 6")
        page.click('body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_2N-K8 > div > div')
-
+       print(" 7")
        page.click('#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div.zp_3cHPT > div.zp_3Lzj1 > div > div > div > div.finder-explorer-sidebar-shown.zp_1cI0A.zp_iYGsF > div.zp_1ybjt > div > div.zp_p7Ra4 > div > div > div > div > div.zp_3Lry3 > div > div.zp-button-group.zp_3tokH.zp_3HFM6 > div:nth-child(3)')
+       print("25 have successfully added!!")
 
 time.sleep(10)
 
